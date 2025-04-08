07dda75c8792b00af0c5feace0c70da5ee78d238
Yafté M. <redyafte@gmail.com>
2025-04-08 17:39:58 -0600

    feat: added dataclass

--- src/solid_principles/single_responsability/after.py
+++ src/solid_principles/single_responsability/after.py
@@ -11,8 +11,9 @@ _ = load_dotenv()
 
 
 @dataclass
-class PaymentProcessor:
-    def process_transaction(self, customer_data, payment_data) -> Charge:
+class CustomerValidator:
+
+    def validate(self, customer_data):
         if not customer_data.get("name"):
             print("Invalid customer data: missing name")
             raise ValueError("Invalid customer data: missing name")
@@ -21,10 +22,20 @@ class PaymentProcessor:
             print("Invalid customer data: missing contact info")
             raise ValueError("Invalid customer data: missing contact info")
 
+
+@dataclass
+class PaymentDataValidator:
+
+    def validate(self, payment_data):
         if not payment_data.get("source"):
             print("Invalid payment data")
             raise ValueError("Invalid payment data")
 
+
+@dataclass
+class StripePaymentProcessor:
+
+    def process_transaction(self, customer_data, payment_data) -> Charge:
         stripe.api_key = os.getenv("STRIPE_API_KEY")
 
         try:
@@ -39,6 +50,23 @@ class PaymentProcessor:
             print("Payment failed:", e)
             return
 
+        print("Charge: ", charge)
+        return charge
+
+
+@dataclass
+class TransactionLogger:
+
+    def log(self, customer_data, payment_data, charge):
+        with open("transactions.log", "a") as log_file:
+            log_file.write(f"{customer_data['name']} paid {payment_data['amount']}\n")
+            log_file.write(f"Payment status: {charge['status']}\n")
+
+
+@dataclass
+class Notifier:
+
+    def send_confirmation(self, customer_data):
         if "email" in customer_data["contact_info"]:
             # import smtplib
             from email.mime.text import MIMEText
@@ -60,17 +88,40 @@ class PaymentProcessor:
                 f"send the sms using {sms_gateway}: SMS sent to {phone_number}: Thank you for your payment."
             )
 
-        else:
-            print("No valid contact information for notification")
-            return charge
 
-        with open("transactions.log", "a") as log_file:
-            log_file.write(f"{customer_data['name']} paid {payment_data['amount']}\n")
-            log_file.write(f"Payment status: {charge['status']}\n")
+@dataclass
+class PaymentService:
+    customer_validator = CustomerValidator()
+    payment_validator = PaymentDataValidator()
+    payment_processor = StripePaymentProcessor()
+    notifier = Notifier()
+    logger = TransactionLogger()
+
+    def process_transaction(self, customer_data, payment_data) -> Charge:
+        try:
+            self.customer_validator.validate(customer_data)
+        except ValueError as e:
+            raise e
+
+        try:
+            self.payment_validator.validate(payment_data)
+        except ValueError as e:
+            raise e
+
+        try:
+            charge = self.payment_processor.process_transaction(
+                customer_data, payment_data
+            )
+            self.notifier.send_confirmation(customer_data)
+            self.logger.log(customer_data, payment_data, charge)
+            return charge
+        except StripeError as e:
+            print("Payment failed: ", e)
+            raise e
 
 
 if __name__ == "__main__":
-    payment_processor = PaymentProcessor()
+    payment_processor = PaymentService()
 
     customer_data_with_email = {
         "name": "John Doe",
@@ -82,6 +133,18 @@ if __name__ == "__main__":
     }
 
     payment_data = {"amount": 500, "source": "tok_mastercard", "cvv": 123}
+    # payment_processor.process_transaction(customer_data_with_email, payment_data)
+    # payment_processor.process_transaction(customer_data_with_phone, payment_data)
 
-    payment_processor.process_transaction(customer_data_with_email, payment_data)
-    payment_processor.process_transaction(customer_data_with_phone, payment_data)
+    # Payment with error
+    payment_data_with_error = {
+        "amount": 700,
+        "source": "tok_visa_chargeDeclined",
+        "cvv": 123,
+    }
+    try:
+        payment_processor.process_transaction(
+            customer_data_with_email, payment_data_with_error
+        )
+    except Exception as e:
+        print(f"Error con el procesamiento: {e}")
