e77b321b3a37b6f211a50b6019f28ca20f288c21
Yafté M. <redyafte@gmail.com>
2025-04-14 13:41:45 -0600

    feat: added commons in payment service

---
+++ src/payment_service/commons/customer.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from .contact import ContactInfo
+
+
+class CustomerData(BaseModel):
+    name: str
+    contact_info: ContactInfo
+    customer_id: Optional[str] = None
