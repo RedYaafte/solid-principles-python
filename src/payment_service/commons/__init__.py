e910bdfead92a0d0b4af9bc9e801d74f0a16673e
Yafté M. <redyafte@gmail.com>
2025-04-14 16:26:54 -0600

    feat: added PaymentTypo to init

--- src/payment_service/commons/__init__.py
+++ src/payment_service/commons/__init__.py
@@ -1,7 +1,13 @@
 from .contact import ContactInfo
 from .customer import CustomerData
-from .payment_data import PaymentData
+from .payment_data import PaymentData, PaymentType
 from .payment_response import PaymentResponse
 
 
-__all__ = ["ContactInfo", "CustomerData", "PaymentData", "PaymentResponse"]
+__all__ = [
+    "ContactInfo",
+    "CustomerData",
+    "PaymentData",
+    "PaymentType",
+    "PaymentResponse",
+]
